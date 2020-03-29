126ce17ab8ef5fa52613d21ef36b7710ca07c331
Alejandro9415 <45185149+Alejandro9415@users.noreply.github.com>
2020-03-29 18:09:30 -0500

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/alejoo/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/alejoo/Documents/flutter/ux"
+export "FLUTTER_TARGET=/Users/alejoo/Documents/flutter/ux/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/alejoo/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
